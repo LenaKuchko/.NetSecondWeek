fefae848306352f58963faf61a6a3e4b78a9af6a
Olena Kuchko <kuchkoel@gmail.com>
2017-08-11 13:47:22 -0700

    add model test

---
+++ SeafoodMarketTest.cs
@@ -0,0 +1,21 @@
+﻿using SeafoodMarket.Models;
+using Xunit;
+
+namespace SeafoodMarket.Tests
+{
+    public class NewsletterTest
+    {
+        [Fact]
+        public void GetEmailTest()
+        {
+            //Arrange
+            var newsletter = new Newsletter();
+
+            //Act
+            var result = item.Email;
+
+            //Assert
+            Assert.Equal("lena@gmail.com", result);
+        }
+    }
+}
